74f66b8d8f4dccd0ed9ea5d31b9927c35cc15f90
damiankryzia71 <damiankryzia71@gmail.com>
2024-03-21 12:42:42 -0700

    First version of CRUD methods

--- inventory_rental_cs/models.py
+++ inventory_rental_cs/models.py
@@ -1,43 +1,207 @@
-from django.db import models
+class Account():
+    def __init__(self, id, username, password, email):
+        #Crate DB table here if it doesn't exist yet
+        #Set parameters:
+        self.id = id
+        self.username = username
+        self.password = password
+        self.email = email
 
-# Create your models here.
-class Account(models.Model):
-    username = models.CharField(max_length = 50)
-    password = models.CharField(max_length = 50)
-    email = models.EmailField()
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Account()
+
+    #Update the account in the DB
+    def update(self, username = None, password = None, email = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
 
 class Student(Account):
-    balance = models.DecimalField()
-    status = models.CharField()
+    def __init__(self, id, username, password, email, balance, status):
+        super().__init__(id, username, password, email)
+        self.balance = balance
+        self.status = status
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Student()
+
+    #Update the account in the DB
+    def update(self, username = None, password = None, email = None, balance = None, status = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
 
 class Staff(Account):
-    #Since Staff differs from Account only when it comes to user permissions,
-    #only methods will be required here, without separate fields.
-    #For now, an empty method is defined.
-    def empty_method():
-        return None
+    #Constructor is inherited from Account
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Staff()
+
+    #Update the account in the DB
+    def update(self, username = None, password = None, email = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
+
     
-class Item(models.Model):
-    name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 200)
-    quantity = models.IntegerField()
-    category = models.CharField()
+class Item():
+    def __init__(self, id, name, descritpion, quantity, category):
+        self.id = id
+        self.name = name
+        self.description = descritpion
+        self.quantity = quantity
+        self.category = category
 
-class Cart(models.Model):
-    studentId = models.ForeignKey(Student, on_delete = models.CASCADE)
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
 
-class CartItem(models.Model):
-    quantity = models.IntegerField()
-    cartId = models.ForeignKey(Cart, on_delete = models.CASCADE)
-    itemId = models.ForeignKey(Item, on_delete = models.CASCADE)
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Item()
 
-class Rental(models.Model):
-    pickup_date_time = models.DateTimeField()
-    return_date_time = models.DateTimeField()
-    status = models.CharField()
-    studentId = models.ForeignKey(Student, on_delete = models.CASCADE)
+    #Update the account in the DB
+    def update(self, name = None, description = None, quantity = None, category = None):
+        pass
 
-class RentalItem(models.Model):
-    status = models.CharField()
-    itemId = models.ForeignKey(Item, on_delete = models.CASCADE)
-    rentalId = models.ForeignKey(Rental, on_delete = models.CASCADE)
+    #Archive the account
+    def delete(self):
+        pass
+
+class Cart():
+    def __init__(self, id, studentId):
+        self.id = id
+        self.studentId = studentId
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Cart()
+
+    #Update the account in the DB
+    def update(self, studentId = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
+
+class CartItem():
+    def __init__(self, id, quantity, cartId, itemId):
+        self.id = id
+        self.quantity = quantity
+        self.cartId = cartId
+        self.itemId = itemId
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return CartItem()
+
+    #Update the account in the DB
+    def update(self, quantity = None, cartId = None, itemId = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
+
+class Rental():
+    #Comment on datetimes: we will use a date time object instead of a raw string, and use a toString(or whatever it's called)
+    #method so that every date time has the same format in the DB
+    def __init__(self, id, pickup_date_time, return_date_time, status, studentId):
+        self.id = id
+        self.pickup_date_time = pickup_date_time
+        self.return_date_time = return_date_time
+        self.status = status
+        self.studentId = studentId
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return Rental()
+
+    #Update the account in the DB
+    def update(self, pickup_date_time = None, return_date_time = None, status = None, studentId = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
+
+class RentalItem():
+    def __init__(self, id, status, itemId, rentalId):
+        self.id = id
+        self.status = status
+        self.itemId = itemId
+        self.rentalId = rentalId
+
+    #Create an account in DB
+    #Uses this class instance's fields
+    def create(self):
+        pass
+
+    #Static: read from the database based on arguments and return an instance of this class
+    #A separate function to build the SQL query from kwargs will be used
+    @staticmethod
+    def read(**kwargs):
+        return RentalItem()
+
+    #Update the account in the DB
+    def update(self, status = None, itemId = None, rentalId = None):
+        pass
+
+    #Archive the account
+    def delete(self):
+        pass
