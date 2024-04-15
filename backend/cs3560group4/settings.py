39420d4eadbc917e42c540fc7469d1633371fccc
Tony Gonzalez <106697055+tonygonzalez14@users.noreply.github.com>
2024-04-14 21:30:27 -0700

    updating branch

--- backend/cs3560group4/settings.py
+++ backend/cs3560group4/settings.py
@@ -75,15 +75,14 @@ WSGI_APPLICATION = "cs3560group4.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "cs3560_group4",
         "USER": "postgres",
-        "PASSWORD": "udemy",
-        "HOST": "192.168.50.45",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
         "PORT": "5432"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
