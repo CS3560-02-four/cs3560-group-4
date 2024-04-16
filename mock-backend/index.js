64495587cda5b329c7d99d37e26e22315cf8cb45
damiankryzia71 <damiankryzia71@gmail.com>
2024-04-16 16:12:37 -0700

    set up db connection api to use while developing frontend

---
+++ mock-backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import pg from "pg";
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "inventory_rental",
+    password: "udemy",
+    port: 5432
+});
+await db.connect();
+
+app.get("/", async (req, res) => {
+    const query = req.body.query;
+    const result = await db.query(query);
+    res.send(result.rows);
+});
+
+app.listen(8000, () => {
+    console.log("Mock backend listening on port 8000.");
+});
