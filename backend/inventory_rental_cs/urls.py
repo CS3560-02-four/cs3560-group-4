ccb8d9cf77b0a072502f3dea0d8af40bbb3ab3b3
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-04-19 23:56:57 -0700

    delete tests

--- backend/inventory_rental_cs/urls.py
+++ backend/inventory_rental_cs/urls.py
@@ -7,17 +7,4 @@ urlpatterns = [
     path("accountInfo/", views.getAccountDetails), # Gets account info, use ?account_id=___ in url
     path("rentalDetails/", views.getUserRentals),  # Gets all rentals associated with a user, use ?account_id=___ in url
     path("createAppointment/", views.createRental), # Creates a rental using a user's cart, use ?account_id=___ in url
-    path("laptops/", views.test),
-    path("allitems/", views.test2),
-    path("insertcartitem/", views.test3),
-    path("cartiteminfo/", views.test4),
-    path("insertitem/", views.test5),
-    path("rentalUpdate/", views.test6),
-    path("rentalDelete/", views.test7),
-    path("rentalItemInsert/", views.test8),
-    path("rentalItemUpdate/<account_id>/", views.test9),
-    path("rentalItemDelete/", views.test10),
-    path("getRental/", views.test13),
-    path("getRentalsJson/", views.test14),
-    path("exampleJson/", views.test_json) #USE THIS FOR THE EXAMPLE JSON METHOD
 ]
