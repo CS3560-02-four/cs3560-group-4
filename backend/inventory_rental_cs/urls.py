6ecc0091c8e680ae4916711cead09bf8017915bd
miloszkryzia1 <milosz.kryzia@gmail.com>
2024-04-19 20:48:45 -0700

    rename url

--- backend/inventory_rental_cs/urls.py
+++ backend/inventory_rental_cs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     #urls for testing
-    path("getcartitems/", views.get_available_items),
+    path("get-available-items/", views.get_available_items), #DONE, get all items available for rental and their quantities
     path("exampleJson/", views.test_json) #USE THIS FOR THE EXAMPLE JSON METHOD
 ]
