50d43e5358e9ef952e45151c3860474d44e834a8
damiankryzia71 <damiankryzia71@gmail.com>
2024-04-27 20:41:25 -0700

    add category selection to new item form

--- frontend/src/app/ui/admin/admin-inventory/NewItemForm.tsx
+++ frontend/src/app/ui/admin/admin-inventory/NewItemForm.tsx
@@ -27,11 +27,21 @@ export default function NewItemForm() {
     return (
         <form action="" className="flex flex-col gap-4 shadow-md px-5 py-7 fixed left-[75%]">
             <div className="self-center text-xl text-green-900">Add an Item</div>
-            <input type="text" placeholder="Item Name" value={name} onChange={onNameChange} name="name" />
-            <textarea className="resize-auto" cols={30} rows={8} placeholder="Item Description" onChange={onDescriptionChange} name="description">{description}</textarea>
+            <input className="p-2 shadow-md" type="text" placeholder="Item Name" value={name} onChange={onNameChange} name="name" />
+            <textarea className="resize-none p-2 shadow-md" cols={30} rows={8} placeholder="Item Description" onChange={onDescriptionChange} name="description">{description}</textarea>
             <div className="flex gap-5 self-center">
                 <label htmlFor="quantity">Quantity:</label>
-                <input className="w-5 text-center bg-gray-200 rounded text-[17px]" type="text" value={quantity} onChange={onQuantityChange} name="quantity" />
+                <input className="w-5 text-center shadow-md rounded text-[17px]" type="text" value={quantity} onChange={onQuantityChange} name="quantity" />
+            </div>
+            <div className="flex gap-5 self-center">
+                <label htmlFor="category">Category:</label>
+                <select name="category" className="text-green-900">
+                    <option value="Laptops">Laptops</option>
+                    <option value="Calculators">Calculators</option>
+                    <option value="Headsets">Headsets</option>
+                    <option value="Chargers">Chargers</option>
+                    <option value="Mice">Mice</option>
+                </select>
             </div>
             <input type="submit" value="Submit" className="bg-green-900 text-white rounded text-sm p-2 font-medium cursor-pointer" />
         </form>
