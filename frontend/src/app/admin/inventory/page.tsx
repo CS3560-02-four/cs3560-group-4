eefd420303991bd24115e95198c787da6276b868
damiankryzia71 <damiankryzia71@gmail.com>
2024-04-27 19:11:48 -0700

    create add new item form

--- frontend/src/app/admin/inventory/page.tsx
+++ frontend/src/app/admin/inventory/page.tsx
@@ -4,16 +4,18 @@ import { Item } from "@/app/lib/interfaces";
 import { cookies } from "next/headers";
 import AdminItemField from "@/app/ui/admin/admin-inventory/AdminItemField";
 import { redirect } from "next/navigation";
+import NewItemForm from "@/app/ui/admin/admin-inventory/NewItemForm";
 
 export default async function Page() {
     const response = await fetchAllItemsAdmin();
     const items: Array<Item> = response.data;
     if (cookies().has('admin')) {
         return (
-            <div className="relative top-32">
+            <div className="relative top-32 flex justify-between px-20">
                 <div className="flex flex-col gap-9 w-[60%]">
                     {items.map((item: Item) => <AdminItemField key={item.id} item={item} />)}
                 </div>
+                <NewItemForm />
             </div>
         );
     }
