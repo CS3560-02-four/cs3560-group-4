79a811453ed3dc6ebe9478f6a7adb32c1406db02
damiankryzia71 <damiankryzia71@gmail.com>
2024-04-30 20:37:54 -0700

    fix item mark not damaged on rental page

--- frontend/src/app/admin/rentals/[rental]/page.tsx
+++ frontend/src/app/admin/rentals/[rental]/page.tsx
@@ -9,6 +9,16 @@ import { formatDatetime } from "@/app/lib/utils";
 import Link from "next/link";
 
 export default async function Page({ params }: { params: { rental: string } }) {
+
+    function sortFn(a: ItemUnit, b: ItemUnit) {
+        if (a.status === "normal" && b.status === "damaged")
+            return -1;
+        else if (a.status === "damaged" && b.status === "normal")
+            return 1;
+        else
+            return 0;
+    }
+
     const response: DataResponse = await fetchAllRentals();
     const rental: AdminRental = response.data.filter((data: any) => data.id === parseInt(params.rental))[0];
     if (cookies().has('admin')) {
@@ -24,7 +34,7 @@ export default async function Page({ params }: { params: { rental: string } }) {
                     <Link href="/admin/rentals"><button className="text-white rounded text-l p-2 font-medium bg-green-900">Back to All Rentals</button></Link>
                 </div>
                 <div className="flex flex-col gap-9 w-[60%]">
-                    {rental.itemUnits?.map((unit: ItemUnit) => <AdminRentalItemField key={unit.id} itemUnit={unit} />)}
+                    {rental.itemUnits?.sort(sortFn).map((unit: ItemUnit) => <AdminRentalItemField key={unit.id} itemUnit={unit} />)}
                 </div>
             </div>
         );
--- frontend/src/app/ui/admin/admin-rentals/AdminRentalItemField.tsx
+++ frontend/src/app/ui/admin/admin-rentals/AdminRentalItemField.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ItemUnit } from "@/app/lib/interfaces";
-import { markItemDamaged } from "@/app/lib/data";
+import { markItemDamaged, markItemNotDamaged } from "@/app/lib/data";
 import { useState } from "react";
 
 export default function AdminRentalItemField({ itemUnit }: { itemUnit: ItemUnit }) {
@@ -12,12 +12,17 @@ export default function AdminRentalItemField({ itemUnit }: { itemUnit: ItemUnit 
         setDamaged(true);
     }
 
+    function onUnmarkDamaged() {
+        markItemNotDamaged(itemUnit.id);
+        setDamaged(false);
+    }
+
     return (
         <div className="rounded shadow-md p-4 flex flex-col gap-3">
             <div className="text-green-900 text-2xl font-medium">{itemUnit.name}</div>
             <div className="text-green-900 text-xl font-medium">Unit #{itemUnit.id}</div>
             <div className={`${statusTextColor} text-xl font-medium`}>Status: {damaged ? "Damaged" : "Normal"}</div>
-            {!damaged ? <button className="text-white rounded text-l p-2 font-medium bg-yellow-600 w-[20%]" onClick={onMarkDamaged}>Mark as Damaged</button> : null}
+            {!damaged ? <button className="text-white rounded text-l p-2 font-medium bg-yellow-600 w-[20%]" onClick={onMarkDamaged}>Mark as Damaged</button> : <button className="text-white rounded text-l p-2 font-medium bg-green-900 w-[20%]" onClick={onUnmarkDamaged}>Mark as Not Damaged</button>}
         </div>
     );
 }
